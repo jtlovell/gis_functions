1d07018a3c5bae8073855717289c66a504a61cc4
John T. Lovell <johntlovell@gmail.com>
2015-12-08 15:15:44 -0600

    now able to make inset

--- raster2hist_implementation.R
+++ raster2hist_implementation.R
@@ -4,11 +4,11 @@ r <- raster(filename)
 plot(r, main='RasterLayer from file')
 
 # for data that is continuous
-raster2hist(r=r,levels=seq(100,2000,100))
+raster2hist(r=r,levels=seq(100,2000,10))
 raster2hist(r=r,levels=20)
 raster2hist(r=r,levels=20, main="Test")
 raster2hist(r=r,levels=20, main="Test", xlab="value", ylab="count")
-raster2hist(r=r,levels=20, border=F,palette=rainbow)
+raster2hist(r=r,levels=50, border=F)
 
 # or if you want full customizability - return just the table and make a barplot from that. 
 h<-raster2hist(r=r,levels=20, border=F,palette=rainbow, plot=F)
@@ -17,4 +17,22 @@ barplot(h$table)
 # for data that is discrete
 r2 <- cut(raster(filename),10)
 raster2hist(r=r2, levels="discrete", palette=rainbow)
+
+# add the bar plot to a pre-existing plot
+library(TeachingDemos)
+plot(r, main='RasterLayer from file', col=terrain.colors(200))
+subplot( # add plot to existing plot
+  raster2hist(r=r,levels=100,palette=terrain.colors, 
+              border=NA, # turn off bar borders
+              xaxt="n", # turn off x axis
+              axisnames=F, # turn off axis labels
+              space=0 # remove spacing between bars - may need to be negative with lots of bars
+  ),
+  x=extent(r)[1]+400, # place the top of the plot here
+  y=extent(r)[4]+200, # place the left edge of the plot here
+  vadj=1, hadj=0, # ignore
+  size=c(3,1) # set the size of the inset by inches (x, y)
+)
+
+
 
