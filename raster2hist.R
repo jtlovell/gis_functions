c96278f54656477e9d49206c2fcdf0d411ca5a0a
John T. Lovell <johntlovell@gmail.com>
2015-12-07 19:02:44 -0600

    Initial

---
+++ raster2hist.R
@@ -0,0 +1,16 @@
+raster2hist<-function(r, levels="discrete", palette=terrain.colors,...){
+  mat<-as.numeric(as.matrix(r))
+  if(levels[1]=="discrete"){
+    fac<-mat[!is.na(mat)]
+    tab<-table(fac)
+  }else{
+    fac<-cut(mat, breaks=levels)
+    labs <- levels(fac)
+    lower = as.numeric( sub("\\((.+),.*", "\\1", labs) )
+    labs<-lower
+    tab<-table(fac)
+    names(tab)<-labs
+  }
+  pal<-palette(length(unique(fac)))
+  bp<-  barplot (tab, col=pal,...)
+}
---
+++ raster2hist_implementation.R
@@ -0,0 +1,13 @@
+require(raster)
+filename <- system.file("external/test.grd", package="raster")
+r <- raster(filename)
+plot(r, main='RasterLayer from file')
+raster2hist(r=r,levels=seq(100,2000,100))
+raster2hist(r=r,levels=20)
+raster2hist(r=r,levels=20, main="Test")
+raster2hist(r=r,levels=20, main="Test", xlab="value", ylab="count")
+raster2hist(r=r,levels=20, border=F,palette=rainbow)
+
+r2 <- cut(raster(filename),10)
+raster2hist(r=r2, levels="discrete" palette=rainbow)
+
